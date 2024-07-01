0fdd83a11fabb50c2c0e592703b223022e05b74c
Aman Gupta <eagleax7@gmail.com>
2024-07-01 20:25:32 +0530

    deploy on ec2 with ci-cd pipeline

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -21,13 +21,13 @@ export default function Post({ post }) {
         <img className="postImg" src={`${PF}${post.photo}`} alt={post.title} />
       )}
       <div className="postInfo">
-        {/* <div className="postCats">
+        <div className="postCats">
           {post?.categories?.map((c, index) => (
             <span key={index} className="postCat">
               {c}
             </span>
           ))}
-        </div> */}
+        </div>
         <Link to={`/post/${post._id}`} className="link">
           <span className="postTitle">{post.title}</span>
         </Link>
